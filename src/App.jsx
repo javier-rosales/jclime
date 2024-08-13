5952f2403111db085ada7463d582c71ce9856da4
Javier Rosales <cyjav.dev@gmail.com>
2024-08-13 14:44:23 -0600

    Integrate weatherService to fetch weather data and set up basic state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import weatherService from './services/weather'
 
 function App() {
+  const [location, setLocation] = useState("19.680691, -99.257362") // Test fixed location
+  const [weatherData, setWeatherData] = useState(null)
+
+  useEffect(() => {
+    weatherService
+      .getFilteredData(location)
+      .then(retrievedData => {
+        setWeatherData(retrievedData)
+      })
+  }, [location])
+
+  console.log(weatherData)
+
   return (
     <div>
       Coming soon...
